490e29d565468af277952b90dad610ffaf1e2311
tmwatchanan <tmwatchanan@gmail.com>
2017-12-11 18:27:19 +0700

    Refactor and modify get parts of file name

--- ga-train-nn/preprocess.m
+++ ga-train-nn/preprocess.m
@@ -1,10 +1,13 @@
-wdbc_fid = fopen ('wdbc.data', 'r');
-output_fid = fopen ('wdbc_numeric.data', 'w');
+input_file_name = "wdbc.data";
+output_file_name = [strsplit(input_file_name, ".")(1) "-numeric." strsplit(input_file_name, ".")(2)];
+output_file_name = [output_file_name{:}];
+wdbc_fid = fopen (input_file_name, "r");
+output_fid = fopen (output_file_name, "w");
 
 line = fgetl(wdbc_fid);
 while (line != -1)
-  line(line == 'B') = '0';
-  line(line == 'M') = '1';
+  line(line == "B") = "0";
+  line(line == "M") = "1";
   fdisp(output_fid, line);
   line = fgetl(wdbc_fid);
 endwhile
