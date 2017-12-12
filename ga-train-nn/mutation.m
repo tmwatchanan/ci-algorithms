6d4041ec4bcaab86a9388d16f8e0c179447ac02d
tmwatchanan <tmwatchanan@gmail.com>
2017-12-12 21:28:47 +0700

    Forget define min and max of random vvariables

--- ga-train-nn/mutation.m
+++ ga-train-nn/mutation.m
@@ -1,3 +1,5 @@
+minAddedWeight = -1;
+maxAddedWeight = 1;
 for mutated = 1:NUM_CHROMOSOMES
   for layer = 2:NUM_LAYERS
     randomAdded = uniform_random(minAddedWeight, maxAddedWeight, NUM_NODES_IN_LAYER(layer) - 1, NUM_NODES_IN_LAYER(layer-1));
