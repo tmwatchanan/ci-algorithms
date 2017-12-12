05677545fdc5ebb7a06cbe22bd29a109eaa27a8b
tmwatchanan <tmwatchanan@gmail.com>
2017-12-12 20:50:25 +0700

    Add strong mutation

--- ga-train-nn/mutation.m
+++ ga-train-nn/mutation.m
@@ -1,6 +1,9 @@
 for mutated = 1:NUM_CHROMOSOMES
   for layer = 2:NUM_LAYERS
     randomAdded = uniform_random(minAddedWeight, maxAddedWeight, NUM_NODES_IN_LAYER(layer) - 1, NUM_NODES_IN_LAYER(layer-1));
+    randomMutated = uniform_random(minAddedWeight, maxAddedWeight, NUM_NODES_IN_LAYER(layer) - 1, NUM_NODES_IN_LAYER(layer-1));
+    randomMutated = randomMutated < MUTATION_RATE;
+    randomAdded = randomAdded .* randomMutated;
     weight_children{mutated}{layer} = weight_children{mutated}{layer} + randomAdded;
   endfor
 %  chromosome_child = create_chromosome_vector (weight_child, NUM_LAYERS);
