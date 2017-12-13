87e6ffc42687ecaac8b9a3b5486fc015e38b1604
tmwatchanan <tmwatchanan@gmail.com>
2017-12-13 23:37:44 +0700

    Rename figure name

--- fuzzy-logic/plot_fog_density_membership_functions.m
+++ fuzzy-logic/plot_fog_density_membership_functions.m
@@ -4,13 +4,13 @@ x = 0:0.1:3;
 Thin = trapezoidal_shaped (x, Thin_params, e);
 Medium = trapezoidal_shaped (x, Medium_params, e);
 Thick = trapezoidal_shaped (x, Thick_params, e);
-figure('Name', 'Membership Functions of Fog Fuzzy Sets', "Position", [0,0,1000,500], 'NumberTitle', 'off');
+figure('Name', 'Membership Functions of Fog Density Fuzzy Sets', "Position", [0,0,1000,500], 'NumberTitle', 'off');
 plot(x,Thin, 'LineWidth', 2, ['r;Thin fuzzy set params = '  mat2str(Thin_params) ';']);
 hold on;
 plot(x,Medium, 'LineWidth', 2, ['g;Medium fuzzy set params = '  mat2str(Medium_params) ';']);
 hold on;
 plot(x,Thick, 'LineWidth', 2, ['b;Thick fuzzy set params = '  mat2str(Thick_params) ';']);
-title('Membership Function of Thin, Medium and Thick Fog Fuzzy Set', 'fontsize', 16)
+title('Membership Function of Fog Density Fuzzy Set', 'fontsize', 16)
 xlabel('LWC (g/m^3)', 'FontWeight', 'bold', 'fontsize', 14);
 ylabel('Degree of Membership', 'FontWeight', 'bold', 'fontsize', 14);
 ylim([-0.00 1.15]);
