f543e3ddf2cf35f5abf0eadc6731f15373a7509a
tmwatchanan <tmwatchanan@gmail.com>
2017-12-14 00:47:37 +0700

    Rename global variables to solve conflicts (same name)

--- fuzzy-logic/global_configs.m
+++ fuzzy-logic/global_configs.m
@@ -2,11 +2,11 @@
 global e = 1;
 
 % membership functions of light level fuzzy sets
-global VD_params = [0 0 50 200];
-global D_params = [100 150 900 950];
-global M_params = [900 950 2000 2100];
-global B_params = [2000 2100 10000 11000];
-global VB_params = [10000 11000 12000 12001];
+global VeryDark_params = [0 0 50 200];
+global Dark_params = [100 150 900 950];
+global Medium_params = [900 950 2000 2100];
+global Bright_params = [2000 2100 10000 11000];
+global VeryBright_params = [10000 11000 12000 12001];
 
 % membership functions of fog density fuzzy sets
 global Thin_params = [0 0 0.02 0.05];
@@ -14,11 +14,11 @@ global Medium_params = [0.03 0.08 0.25 0.3];
 global Thick_params = [0.20 0.3 3.0 3.1];
 
 % membership functions of brightness fuzzy sets
-global VL_params = [0 0 5 20];
-global L_params = [5 20 30 45];
-global M_params = [30 45 55 70];
-global H_params = [55 70 80 95];
-global VH_params = [80 95 100 101];
+global VeryLow_params = [0 0 5 20];
+global Low_params = [5 20 30 45];
+global Moderate_params = [30 45 55 70];
+global High_params = [55 70 80 95];
+global VeryHigh_params = [80 95 100 101];
 
 % file configs
 global SAVE_FIGURE_DIRNAME = 'figures';
