1beffb688758084fee843f401cf25f9468faca59
tmwatchanan <tmwatchanan@gmail.com>
2017-12-13 21:22:28 +0700

    Refactor and add labels

---
+++ fuzzy-logic/plot_environment_illumination_membership_function.m
@@ -0,0 +1,18 @@
+x = 0:0.1:3;
+Thin_params = [0 0 0.02 0.05];
+Medium_params = [0.03 0.08 0.25 0.3];
+Thick_params = [0.20 0.3 3.0 3.1];
+Thin = trapezoidal_shaped (x, Thin_const, 1);
+Medium = trapezoidal_shaped (x, Medium_const, 1);
+Thick = trapezoidal_shaped (x, Thick_const, 1);
+figure('Name', 'Membership Functions of Fog Fuzzy Sets', "Position", [0,0,1000,500], 'NumberTitle', 'off');
+plot(x,Thin, 'LineWidth', 2, ['r;Thin fuzzy set params = '  mat2str(Thin_params) ';']);
+hold on;
+plot(x,Medium, 'LineWidth', 2, ['g;Medium fuzzy set params = '  mat2str(Medium_params) ';']);
+hold on;
+plot(x,Thick, 'LineWidth', 2, ['b;Thick fuzzy set params = '  mat2str(Thick_params) ';']);
+title('Membership Function of Thin, Medium and Thick Fog Fuzzy Set', 'fontsize', 16)
+xlabel('LWC (g/m^3)', 'FontWeight', 'bold', 'fontsize', 14);
+ylabel('Degree of Membership', 'FontWeight', 'bold', 'fontsize', 14);
+ylim([-0.00 1.15]);
+grid;
