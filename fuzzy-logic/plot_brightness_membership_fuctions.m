592f5a4d02db146a1a33639b24cc42b961ea27cc
tmwatchanan <tmwatchanan@gmail.com>
2017-12-13 22:01:50 +0700

    Rename figure label and title

--- fuzzy-logic/plot_brightness_membership_fuctions.m
+++ fuzzy-logic/plot_brightness_membership_fuctions.m
@@ -9,7 +9,7 @@ L = trapezoidal_shaped (x, L_params, 1);
 M = trapezoidal_shaped (x, M_params, 1);
 H = trapezoidal_shaped (x, H_params, 1);
 VH = trapezoidal_shaped (x, VH_params, 1);
-figure('Name', 'Membership Functions of Brightness Fuzzy Sets', "Position", [0,0,1000,500], 'NumberTitle', 'off');
+figure('Name', 'Membership Functions of Screen Brightness Fuzzy Sets', "Position", [0,0,1000,500], 'NumberTitle', 'off');
 plot(x,VL, 'LineWidth', 2, ['r;Thin fuzzy set params = '  mat2str(VL_params) ';']);
 hold on;
 plot(x,L, 'LineWidth', 2, ['g;Medium fuzzy set params = '  mat2str(L_params) ';']);
@@ -19,7 +19,7 @@ hold on;
 plot(x,H, 'LineWidth', 2, ['c;Thick fuzzy set params = '  mat2str(H_params) ';']);
 hold on;
 plot(x,VH, 'LineWidth', 2, ['m;Thick fuzzy set params = '  mat2str(VH_params) ';']);
-title('Membership Function of Brightness Fuzzy Sets', 'fontsize', 16)
+title('Membership Function of Screen Brightness Fuzzy Sets', 'fontsize', 16)
 xlabel('Brightness Level (%)', 'FontWeight', 'bold', 'fontsize', 14);
 ylabel('Degree of Membership', 'FontWeight', 'bold', 'fontsize', 14);
 ylim([-0.00 1.25]);
