9858e5d724ad38f7969208f1273e2406843f4328
tmwatchanan <tmwatchanan@gmail.com>
2017-12-14 21:24:07 +0700

    Remove redundant codes

--- fuzzy-logic/AdaptiveBrightnessDisplay.m
+++ fuzzy-logic/AdaptiveBrightnessDisplay.m
@@ -5,27 +5,6 @@ global_configs;
 input_fog_density_value = 0;
 input_light_level_value = 4900;
 
-% define fuzzy rules
-fuzzy_rules = [];
-
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thin","VeryDark","VeryLow");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thin","Dark","Low");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thin","Medium","Moderate");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thin","Bright","High");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thin","VeryBright","VeryHigh");
-
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Medium","VeryDark","Moderate");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Medium","Dark","Moderate");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Medium","Medium","High");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Medium","Bright","High");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Medium","VeryBright","VeryHigh");
-
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thick","VeryDark","VeryHigh");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thick","Dark","VeryHigh");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thick","Medium","VeryHigh");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thick","Bright","VeryHigh");
-fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "Thick","VeryBright","VeryHigh");
-
 for r = 1:size(fuzzy_rules, 1)
   fog_density_membership_value = fog_density_membership_function...
                           (fuzzy_rules(r).fog_density, input_fog_density_value);
