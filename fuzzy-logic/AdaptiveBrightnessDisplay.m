e4234ceefd171d420ba270a9613050972b82e9ea
tmwatchanan <tmwatchanan@gmail.com>
2017-12-14 10:39:09 +0700

    Add fuzzy rules to the system

--- fuzzy-logic/AdaptiveBrightnessDisplay.m
+++ fuzzy-logic/AdaptiveBrightnessDisplay.m
@@ -4,12 +4,35 @@ global_configs;
 
 input_fog_density_fuzzy_set = "thin";
 input_fog_density_value = 0.03;
-fog_density_membership_value = fog_density_membership_function (input_fog_density_fuzzy_set, input_fog_density_value)
+fog_density_membership_value = fog_density_membership_function...
+                          (input_fog_density_fuzzy_set, input_fog_density_value)
+                          
+input_light_level_fuzzy_set = "bright";
+input_light_level_value = 5000;
+light_level_membership_value = light_level_membership_function...
+                          (input_light_level_fuzzy_set, input_light_level_value)
 
 input_brightness_fuzzy_set = "moderate";
 input_brightness_value = 50;
-brightness_membership_value = brightness_membership_function (input_brightness_fuzzy_set, input_brightness_value)
+brightness_membership_value =brightness_membership_function...
+                            (input_brightness_fuzzy_set, input_brightness_value)
 
-input_light_level_fuzzy_set = "bright";
-input_light_level_value = 5000;
-light_level_membership_value = light_level_membership_function (input_light_level_fuzzy_set, input_light_level_value)
+fuzzy_rules = [];
+
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thin","very_dark","very_low");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thin","dark","low");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thin","medium","moderate");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thin","bright","high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thin","very_bright","very_high");
+
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "medium","very_dark","moderate");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "medium","dark","moderate");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "medium","medium","high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "medium","bright","high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "medium","very_bright","very_high");
+
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thick","very_dark","very_high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thick","dark","very_high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thick","medium","very_high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thick","bright","very_high");
+fuzzy_rules = add_fuzzy_rule(fuzzy_rules, "thick","very_bright","very_high");
---
+++ fuzzy-logic/add_fuzzy_rule.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2017 Watchanan
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} add_fuzzy_rule (@var{fuzzy_rules},...
+## @var{input_fuzzy_set_1}, @var{input_fuzzy_set_2}, @var{output_fuzzy_set})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Watchanan <Watchanan@WATCHANAN-DELL>
+## Created: 2017-12-14
+
+function [fuzzy_rules] = add_fuzzy_rule (fuzzy_rules, input_fuzzy_set_1, input_fuzzy_set_2, output_fuzzy_set)
+  new_rule_struct = struct ("in1", input_fuzzy_set_1, "in2", input_fuzzy_set_2,...
+                            "out", output_fuzzy_set);
+  fuzzy_rules = [fuzzy_rules, new_rule_struct];
+endfunction
