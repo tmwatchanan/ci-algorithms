bca440c96b43c249c88239739a04a2acf6be385f
tmwatchanan <tmwatchanan@gmail.com>
2017-12-15 13:50:35 +0700

    Make classes into one-hot vector form

--- ga-train-nn/preprocess.m
+++ ga-train-nn/preprocess.m
@@ -6,9 +6,18 @@ output_fid = fopen (output_file_name, "w");
 
 line = fgetl(wdbc_fid);
 while (line != -1)
-  line(line == "B") = "0";
-  line(line == "M") = "1";
-  fdisp(output_fid, line);
+  splited_line = strsplit (line, ",");
+  if any(line == "B")
+    splited_line(1) = "1";
+    splited_line(2) = "0";
+  elseif any(line == "M")
+    splited_line(1) = "0";
+    splited_line(2) = "1";
+  else
+    error("Invalid specific classes");
+  endif
+  joined_line = strjoin (splited_line, ',');
+  fdisp(output_fid, joined_line);
   line = fgetl(wdbc_fid);
 endwhile
 
